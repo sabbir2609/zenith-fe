5909af6b381149fd9ff82f99a61ca300b0936ab9
Sabbir Hasan Munna <sabbirhasan2999@gmail.com>
2023-12-05 02:25:28 +0600

    theme

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,9 +1,67 @@
+import Image from "next/image";
+import userIcon from "./../../../static/image/user_icon.png";
+import Link from "next/link";
+
 export default function Page() {
-    return (
-        <main>
-            <h1>
-                Register Page
-            </h1>
-        </main>
-    )
+  return (
+    <div className="relative flex flex-col justify-center">
+      <div className="w-full p-6 m-auto  rounded-md shadow-md ring-2 ring-gray-800/50 lg:max-w-xl">
+        <h1 className="text-3xl font-semibold text-center">Register</h1>
+        <form className="space-y-4">
+          <div>
+            <label className="label">
+              <span className="text-base label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Name"
+              className="w-full input input-bordered"
+            />
+          </div>
+          <div>
+            <label className="label">
+              <span className="text-base label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Email Address"
+              className="w-full input input-bordered"
+            />
+          </div>
+          <div>
+            <label className="label">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              className="w-full input input-bordered"
+            />
+          </div>
+          <div>
+            <label className="label">
+              <span className="text-base label-text">Confirm Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              className="w-full input input-bordered"
+            />
+          </div>
+          <div>
+            <button className="btn btn-block">Sign Up</button>
+          </div>
+          <div className="pt-4">
+            Already have an account ?
+            <Link
+              href="#"
+              className="text-blue-600 hover:text-blue-800 hover:underline"
+            >
+              Login
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
--- components/dashboard/common/Navbar.tsx
+++ components/dashboard/common/Navbar.tsx
@@ -1,44 +1,108 @@
-import Theme from "./Theme"
-import Image from "next/image"
+import Theme from "./Theme";
+import Image from "next/image";
+import userIcon from "./../../../static/image/user_icon.png";
 
 export default function Navbar() {
-    return (
-        <div className="navbar bg-base-100">
-            <div className="flex-none">
-                <label htmlFor="my-drawer" className="btn btn-square btn-ghost drawer-button lg:hidden ">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-                </label>
-            </div>
-            <div className="flex-1">
-                <a className="btn btn-ghost text-xl">Zenith</a>
-            </div>
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-none">
+        <label
+          htmlFor="my-drawer"
+          className="btn btn-square btn-ghost drawer-button lg:hidden "
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block w-5 h-5 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </label>
+      </div>
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">Zenith</a>
+      </div>
 
-            <div className="flex-none gap-2">
-                <div className="form-control hidden lg:block">
-                    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-                </div>
-
-                <Theme />
-
-                <div className="dropdown dropdown-end">
-                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                        <div className="w-10 rounded-full">
-                            <Image alt="User Profile Pic" width={200} height={200} src="https://source.unsplash.com/random/200x200/?potrait" />
-                        </div>
-                    </div>
-                    <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-                        <li>
-                            <a className="justify-between">
-                                Profile
-                                <span className="badge">New</span>
-                            </a>
-                        </li>
-                        <li><a>Settings</a></li>
-                        <li><a>Logout</a></li>
-                    </ul>
-                </div>
-            </div>
-
+      <div className="flex-none gap-2">
+        <div className="form-control hidden lg:block">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered w-24 md:w-auto"
+          />
         </div>
-    )
+
+        <div title="theme-change" className="dropdown dropdown-end ">
+          <div tabIndex={0} className="btn gap-1 normal-case btn-ghost">
+            <svg
+              width="20"
+              height="20"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block h-5 w-5 stroke-current md:h-6 md:w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
+              ></path>
+            </svg>
+            <span className="hidden md:inline">Theme</span>
+            <svg
+              width="12px"
+              height="12px"
+              className="ml-1 hidden h-3 w-3 fill-current opacity-60 sm:inline-block"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 2048 2048"
+            >
+              <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+            </svg>
+          </div>
+          <Theme Component={undefined} pageProps={undefined} />
+        </div>
+
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <Image
+                alt="User Profile Pic"
+                width={200}
+                height={200}
+                src={userIcon}
+              />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
