c84fd4f2c9a0bbd09f663161caa32272d5a90990
Sabbir hasan Munna <sabbirhasan2999@gmail.com>
2024-04-15 14:22:39 +0600

    Add @vercel/speed-insights dependency to package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Provider from "@/redux/provider";
 import Setup from "@/components/utils/Setup";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
