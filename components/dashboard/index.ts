85c63db3f516a9e0aa5ec37b687f20127e6845bd
theviriditas <theviriditas@gmail.com>
2025-02-12 23:17:38 +0600

    add initial components and utilities for UI framework

---
+++ components/dashboard/index.ts
@@ -0,0 +1,5 @@
+export { AppSidebar as default } from './app-sidebar';
+export { NavMain as NavMain } from './nav-main';
+export { NavUser as NavUser } from './nav-user';
+export { TeamSwitcher as TeamSwitcher } from './team-switcher';
+export { NavProjects as NavProjects } from './nav-projects';
